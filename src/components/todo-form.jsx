e308413d3bdf856e2c2cc6226c84954ada887a90
awanvirgi <virgiawan.dev15@gmail.com>
2023-10-25 22:45:58 +0700

    Tambahkan Fitur Add List

--- src/components/todo-form.jsx
+++ src/components/todo-form.jsx
@@ -1,16 +1,30 @@
+import { useEffect } from "react"
 import { useState } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch } from "react-redux";
+import { addTodo } from "../redux/reducers/todo-reducers"
 
 function Formtodo() {
     const dispatch = useDispatch();
+    const [ input, setInput ] = useState("")
+
+    const handleInput = (event) => {
+        event.preventDefault()
+        let newToDO = {
+            value: input,
+            finish: false
+        }
+        console.log(newToDO)
+        dispatch(addTodo(newToDO))
+
+    }
     return (
         <div className="mb-10">
             <form className="flex gap-10">
                 <input className="bg-transparent border-b-4 border-blue-400 outline-none text-white grow"
                     placeholder="Masukan Kegiatan"
                     type="text"
-                    />
-                <button className="bg-blue-400 rounded p-2 font-bold text-white" >Tambah +</button>
+                    value={input} onChange={(e) => setInput(e.target.value)} />
+                <button className="bg-blue-400 rounded p-2 font-bold text-white" onClick={handleInput}>Tambah +</button>
             </form>
         </div>
     )
--- src/redux/reducers/todo-reducers.js
+++ src/redux/reducers/todo-reducers.js
@@ -1,22 +1,39 @@
 const initialValue = {
-    todos : [
+    todos: [
         {
-            id:0,
-            value:"Pekerjaan Tugas",
-            finish:false
+            id: 0,
+            value: "Pekerjaan Tugas",
+            finish: false
         },
         {
-            id:1,
-            value:"Project Akhir",
-            finish:true
+            id: 1,
+            value: "Project Akhir",
+            finish: true
         },
     ],
-    isLoading:true
+    isLoading: true
 }
 
-function todoReducer(state = initialValue,action){
-    switch(action.type){
-        default : return state
+function todoReducer(state = initialValue, action) {
+    switch (action.type) {
+        case "addTodo":
+            let newTodos = {
+                id: Date.now(),
+                value: action.payload.value,
+                finish: action.payload.finish
+            }
+            const clone = [...state.todos, newTodos]
+            return {
+                todos: clone
+            }
+        default: return state
     }
 }
+export function addTodo(data) {
+    return {
+        type: "addTodo",
+        payload: data
+    }
+}
+
 export default todoReducer
